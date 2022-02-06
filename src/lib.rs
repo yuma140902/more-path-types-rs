c7fb641812f2060575e9516d4576e92c3ad19884
yuma140902 <yuma140902@gmail.com>
2022-02-06 20:40:09 +0900

    Feat: definitions

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::marker::PhantomData;
+
+pub type StdPath = std::path::Path;
+pub type StdPathBuf = std::path::PathBuf;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Path<RA, T> {
+    inner: StdPathBuf,
+    _phantom_ra: PhantomData<RA>,
+    _phantom_t: PhantomData<T>,
+}
+
+pub struct Absolute;
+pub struct Relative;
+pub struct Both;
+
+pub struct File;
+pub struct Directory;
+pub struct Exist;
+pub struct NotExist;
+pub struct Any;
+
+impl<RA, T> AsRef<StdPath> for Path<RA, T> {
+    fn as_ref(&self) -> &StdPath {
+        &self.inner
     }
+}
+
+pub enum Error {
+    IO { io_error: std::io::Error },
 }
